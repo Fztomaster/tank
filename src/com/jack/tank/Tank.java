de7be679e55a7355a954c8896563f37751f2c23b
fztomaster <929742025@qq.com>
2019-05-30 22:30:54 +0800

    添加坦克静止的处理, 只要四个键都没有按下去, 坦克就是静止的

--- src/com/jack/tank/Tank.java
+++ src/com/jack/tank/Tank.java
@@ -8,6 +8,7 @@ public class Tank {
 	private int y = 200;
 	private Dir dir = Dir.LEFT;
 	private static final int SPEED = 10;
+	private boolean moving = false;
 	
 	public Tank(int x, int y, Dir dir) {
 		super();
@@ -23,25 +24,39 @@ public class Tank {
 	public void setDir(Dir dir) {
 		this.dir = dir;
 	}
+	
+	public boolean isMoving() {
+		return moving;
+	}
+
+	public void setMoving(boolean moving) {
+		this.moving = moving;
+	}
 
 	public void paint(Graphics g) {
-		System.out.println("paint...");
 		g.fillRect(x, y, 50, 50);
+		moving();
+	}
+
+	private void moving() {
+		if (!moving) return;
+		
 		switch (dir) {
-			case LEFT:
-				x -= SPEED;
-				break;
-			case UP:
-				y -= SPEED;
-				break;
-			case RIGHT:
-				x += SPEED;
-				break;
-			case DOWN:
-				y += SPEED;
-				break;
-			default:
-				break;
-		}
+		case LEFT:
+			x -= SPEED;
+			break;
+		case UP:
+			y -= SPEED;
+			break;
+		case RIGHT:
+			x += SPEED;
+			break;
+		case DOWN:
+			y += SPEED;
+			break;
+		default:
+			break;
+	}
+		
 	}
 }
