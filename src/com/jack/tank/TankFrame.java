3014efaf5f6c1b4d15f6f407f8fe7c9d45fa7a79
fztomaster <929742025@qq.com>
2019-05-31 23:53:15 +0800

    设置爆炸的声音

--- src/com/jack/tank/TankFrame.java
+++ src/com/jack/tank/TankFrame.java
@@ -17,6 +17,7 @@ public class TankFrame extends Frame {
 	Tank myTank = new Tank(200, 400, Dir.LEFT, Group.GOOD, this);
 	List<Bullet> bullets = new ArrayList<>();	
 	List<Tank> tanks = new ArrayList<Tank>();
+	Explode e = new Explode(100, 100, this);
 	static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 	
 	public TankFrame() {
@@ -80,7 +81,7 @@ public class TankFrame extends Frame {
 		for (int i = 0; i < tanks.size(); i++) {
 			tanks.get(i).paint(g);
 		}
-		
+		e.paint(g);
 		for (int i = 0; i < bullets.size(); i++) {
 			for (int j = 0; j < tanks.size(); j++) {
 				bullets.get(i).collideWith(tanks.get(j));
