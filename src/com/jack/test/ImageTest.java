8ef846c1a209e949b92dfa866b3f907c7e88c256
fztomaster <929742025@qq.com>
2019-05-31 22:00:55 +0800

    1.加入图片, 2.建立resMgr类管理资源, 把图片load内存, 坦克换成图片

--- src/com/jack/test/ImageTest.java
+++ src/com/jack/test/ImageTest.java
@@ -17,6 +17,10 @@ class ImageTest {
 		try {
 			BufferedImage image = ImageIO.read(new File("F:/code/tank/tank/src/images/bulletD.gif"));
 			assertNotNull(image);
+			
+			BufferedImage image2 = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+			// ImageTest.class ==> this.getClass()
+			assertNotNull(image2);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
