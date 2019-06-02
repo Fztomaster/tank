08314c40316ccb82fdd4a5c41c6927d717c7849d
fztomaster <929742025@qq.com>
2019-06-02 10:03:50 +0800

    boundary check

--- src/com/jack/tank/Tank.java
+++ src/com/jack/tank/Tank.java
@@ -130,6 +130,24 @@ public class Tank {
 		if (this.group == Group.BAD && random.nextInt(100) > 95) {
 			randomDir();
 		}
+		
+		boundsCheck();
+	}
+	// boundary check
+	private void boundsCheck() {
+		if (this.x < 2) {
+			x = 2;
+		}
+		if (this.y < 28) {
+			y = 28;
+		}
+		if (this.x > TankFrame.GAME_WIDTH - Tank.WIDTH - 2) {
+			x = TankFrame.GAME_WIDTH - Tank.WIDTH - 2;
+		}
+		if (this.y > TankFrame.GAME_HEIGHT - Tank.HEIGHT - 2) {
+			y = TankFrame.GAME_HEIGHT - Tank.HEIGHT - 2;
+		}
+		
 	}
 
 	// enemies tank random move direction
