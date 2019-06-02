4668a7611b4d27dae26e42bd44b9e4a31a964264
fztomaster <929742025@qq.com>
2019-06-02 09:58:39 +0800

    好坦克和坏坦克的图片区分

--- src/com/jack/tank/Tank.java
+++ src/com/jack/tank/Tank.java
@@ -16,8 +16,8 @@ public class Tank {
 	private boolean moving = true;
 	private TankFrame tf = null;
 	// 坦克的宽和高
-	public static int WIDTH = ResourceMgr.tankU.getWidth();
-	public static int HEIGHT = ResourceMgr.tankU.getHeight();	
+	public static int WIDTH = ResourceMgr.goodTankU.getWidth();
+	public static int HEIGHT = ResourceMgr.goodTankU.getHeight();	
 	
 	public boolean living = true;
 	private Group group = Group.BAD;
@@ -79,16 +79,16 @@ public class Tank {
 		
 		switch(dir) {
 			case LEFT:
-				g.drawImage(ResourceMgr.tankL, x, y, null);
+				g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankL : ResourceMgr.badTankL, x, y, null);
 				break;
 			case UP:
-				g.drawImage(ResourceMgr.tankU, x, y, null);
+				g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankU : ResourceMgr.badTankU, x, y, null);
 				break;
 			case RIGHT:
-				g.drawImage(ResourceMgr.tankR, x, y, null);
+				g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankR : ResourceMgr.badTankR, x, y, null);
 				break;
 			case DOWN:
-				g.drawImage(ResourceMgr.tankD, x, y, null);
+				g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankD : ResourceMgr.badTankD, x, y, null);
 				break;
 		}
 		move();
